0df91f6ef992b16d1bb4a3fe7406f677fbd7dc5a
alperenkrpnr <alperen.karapinar@metu.edu.tr>
2025-04-03 17:51:34 +0300

    Remove config.py and add it to .gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SECRET_KEY = "some_secret_key"
-
-# Veritabanı adını URI içinde belirtmeyi unutmayın.
-MONGO_URI = "mongodb+srv://alperenkrpnr:MAvhMaIurMuIadmX@alperencluster.ezkuj.mongodb.net/AlperenCluster?retryWrites=true&w=majority&appName=AlperenCluster"
